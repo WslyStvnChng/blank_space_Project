d12cb6c89b795d050ba1fdbc2b337aa7d2826ee7
Tucker <tuckerk23@gmail.com>
2018-03-19 23:12:25 -0600

    updated routes, and model js files to handle db requirements. requires testing with html

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -1,44 +1,30 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our models
 var db = require("../models");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
 
   // GET route for getting all of the posts
   app.get("/api/posts", function(req, res) {
     var query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
+    if (req.query.user_id) {
+      query.UserId = req.query.user_id;
     }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
+    
+    // Get route for finding all posts
     db.Post.findAll({
       where: query,
-      include: [db.Author]
+      include: [db.User]
     }).then(function(dbPost) {
       res.json(dbPost);
     });
   });
 
-  // Get rotue for retrieving a single post
+  // Get rotue for finding a single post
   app.get("/api/posts/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
     db.Post.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Author]
+      include: [db.User]
     }).then(function(dbPost) {
       res.json(dbPost);
     });
