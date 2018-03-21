153593a0596a019d544dde31c4d90ce730eae560
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2018-03-20 19:55:45 -0600

    fixed an age verification this

--- public/js/main.js
+++ public/js/main.js
@@ -9,40 +9,40 @@ $('#year').append('<option value ="' + i + '">' + i + '</option>')
 
 
 $(document).ready(function () {
-+    e.preventDefault();
-+    var age_form = $('#verify .age form');
-+      modal = $('#verify');
-+
-+    if ( localStorage.getItem('isOldEnough') === 'yes' ) {
-+        modal.remove();
-+    }
-+
-+    function meetsMinimumAge(birthDate, minAge) {
-+        var tempDate = new Date(birthDate.getFullYear() + minAge, birthDate.getMonth(), birthDate.getDate());
-+        return (tempDate <= new Date());
-+    }
-+
-+    age_form.submit(function(e){
-+
-+        e.preventDefault();
-+
-+        var $this = $(this),
-+            month = parseInt($this.find('select[name=month]').val(), 10),
-+            day = parseInt($this.find('select[name=day]').val(), 10),
-+            year = parseInt($this.find('select[name=year]').val(), 10),
-+            birthday = new Date(year, month, day);
-+
-+        if ( meetsMinimumAge(birthday, 18) ) {
-+            modal.remove();
-+            localStorage.setItem('isOldEnough', 'yes');
-+        } else {
-+            modal.find('.denied').fadeIn('fast');
-+        }
-+
-+    });
-+
-+    $('#verify')
-+        .height( $(document).height() )
-+        .width( $(document).width() );
-+
-+});
+   // event.preventDefault();
+   var age_form = $('#verify .age form');
+     var modal = $('#verify');
+
+    if ( localStorage.getItem('isOldEnough') === 'yes' ) {
+        modal.remove();
+    }
+
+    function meetsMinimumAge(birthDate, minAge) {
+       var tempDate = new Date(birthDate.getFullYear() + minAge, birthDate.getMonth(), birthDate.getDate());
+        return (tempDate <= new Date());
+   }
+
+    age_form.on("submit",function(e){
+
+        e.preventDefault();
+console.log("Hi Mary");
+
+       var $this = $(this),
+            month = parseInt($this.find('select[name=month]').val(), 10),
+           day = parseInt($this.find('select[name=day]').val(), 10),
+           year = parseInt($this.find('select[name=year]').val(), 10),
+            birthday = new Date(year, month, day);
+
+       if ( meetsMinimumAge(birthday, 18) ) {
+            modal.remove();
+            localStorage.setItem('isOldEnough', 'yes');
+        } else {
+           modal.find('.denied').fadeIn('fast');
+       }
+
+   });
+
+   $('#verify')
+       .height( $(document).height() )
+       .width( $(document).width() );
+});
