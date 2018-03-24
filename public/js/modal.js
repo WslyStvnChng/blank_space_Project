65a6b882be56fd947aaace2bd3e93d2134e84ec8
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-03-24 13:06:22 -0600

    one document.get ready function for various click events per image. Working to get tags to work with require and close button stop working.

--- public/js/modal.js
+++ public/js/modal.js
@@ -3,12 +3,12 @@
 //request the tags for a specific image img/imgId
 //call to route, the method, the callback, inside there is where 
 //we would create the content for these popovers. 
-// var tags = require('../tags.js');
+// var tags = require('../../../models/tags');
 
 
 //Row A [1]
 $(document).ready(function() {
-  // initialize popover with dynamic content
+
   $('[data-toggle="popover-row-a"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
@@ -17,160 +17,136 @@ $(document).ready(function() {
       "<hr><b>User3:</b>  What's gucci mean?" +
       "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
-  });
-});
+    });
+      $("[data-toggle=popover-row-a]").on("shown.bs.popover", function() {
+      $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
+    })
 
-// dismiss close button when clicked
-$("[data-toggle=popover-row-a]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
-});
+
 
 //Row B [2]
-$(document).ready(function() {
   $('[data-toggle="popover-row-b"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
-
-  });
-});
-
-$("[data-toggle=popover-row-b]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
-});
+    });
+      $("[data-toggle=popover-row-b]").on("shown.bs.popover", function() {
+      $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
+    }); 
 
 //Row C [3]
-$(document).ready(function() {
   $('[data-toggle="popover-row-c"]').popover({
-    html: true,
-    title: 'Username: <a class="close" href="#");">&times;</a>',
-    content:
-      "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
-    placement: "auto"
-  });
-});
-
-$("[data-toggle=popover-row-c]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
-});
+      html: true,
+      title: 'Username: <a class="close" href="#");">&times;</a>',
+      content:
+        "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
+        "<hr><b>User3:</b>  What's gucci mean?" +
+        "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
+      placement: "auto"
+    });
+      $("[data-toggle=popover-row-c]").on("shown.bs.popover", function() {
+      $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
+    });
 
 //Row D [4]
-$(document).ready(function() {
   $('[data-toggle="popover-row-d"]').popover({
-    html: true,
-    title: 'Username: <a class="close" href="#");">&times;</a>',
-    content:
-      "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
-    placement: "auto"
-  });
-});
-
-$("[data-toggle=popover-row-d]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
-});
+      html: true,
+      title: 'Username: <a class="close" href="#");">&times;</a>',
+      content:
+        "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
+        "<hr><b>User3:</b>  What's gucci mean?" +
+       "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",       
+      placement: "auto"
+    });
+      $("[data-toggle=popover-row-d]").on("shown.bs.popover", function() {
+      $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
+    });
 
 //Row E [5]
-$(document).ready(function() {
   $('[data-toggle="popover-row-e"]').popover({
-    html: true,
-    title: 'Username: <a class="close" href="#");">&times;</a>',
-    content:
-      "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
-    placement: "auto"
+      html: true,
+      title: 'Username: <a class="close" href="#");">&times;</a>',
+      content:
+        "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
+        "<hr><b>User3:</b>  What's gucci mean?" +
+        "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>", 
+      placement: "auto"
+    });
+      $("[data-toggle=popover-row-e]").on("shown.bs.popover", function() {
+      $(".popover").css("middle", parseInt($(".popover").css("middle")) + -20 + "px");
   });
-});
-
-$("[data-toggle=popover-row-e]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 40 + "px");
-});
 
 //Row F [6]
-$(document).ready(function() {
   $('[data-toggle="popover-row-f"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
   });
-});
-
-$("[data-toggle=popover-row-f]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
-});
-
-//Row G [7]
-$(document).ready(function() {
+    $("[data-toggle=popover-row-f]").on("shown.bs.popover", function() {
+    $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
+  });
+  
+//Row G 7]
   $('[data-toggle="popover-row-g"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
   });
-});
-
-$("[data-toggle=popover-row-g]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 40 + "px");
-});
-
+    $("[data-toggle=popover-row-g]").on("shown.bs.popover", function() {
+    $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
+  });
 //Row H [8]
-$(document).ready(function() {
   $('[data-toggle="popover-row-h"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
   });
-});
-
-$("[data-toggle=popover-row-h]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 40 + "px");
-});
-
-//Row I [9]
-$(document).ready(function() {
+    $("[data-toggle=popover-row-h]").on("shown.bs.popover", function() {
+    $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
+  });
+  //Row I [9]
   $('[data-toggle="popover-row-i"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
     placement: "auto"
   });
-});
-
-$("[data-toggle=popover-row-i]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 40 + "px");
-});
-
-//Row J [10]
-$(document).ready(function() {
+    $("[data-toggle=popover-row-i]").on("shown.bs.popover", function() {
+    $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
+  });
+  //Row J [10]
   $('[data-toggle="popover-row-j"]').popover({
     html: true,
     title: 'Username: <a class="close" href="#");">&times;</a>',
-    placement: "auto",
     content:
       "<b>User1:</b>  You gucci my dude?<br><hr><br><b>User2:</b>  I'm gucci bro, what's up<br><hr><br>" +
-      "<hr><b>User3:</b>  What's gucci mean?",
-   });
-});
-
-$("[data-toggle=popover-row-j]").on("shown.bs.popover", function() {
-  $(".popover").css("middle", parseInt($(".popover").css("middle")) + 50 + "px");
-});
-
-
+      "<hr><b>User3:</b>  What's gucci mean?" +
+      "<form><input type='text'></input><button id='btn-post-button'>Post</button></form>",
+    placement: "auto"
+  });
+    $("[data-toggle=popover-row-j]").on("shown.bs.popover", function() {
+    $(".popover").css("middle", parseInt($(".popover").css("middle")) + 20 + "px");
+  });
+}); //End of object
 
 
 
@@ -180,11 +156,10 @@ $("[data-toggle=popover-row-j]").on("shown.bs.popover", function() {
 
 // var tags = [{
 //   imageUrl: "google.jpg",
-//   associated location on grid: #imagelocation-D3,
+//   // associated location on grid: #imagelocation-D3,
 //   comment: "blahblah",
 //   link: "google.com",
-//   foreignKey: Boolean,
-
+//   foreignKey: true
 // }]
 
 
