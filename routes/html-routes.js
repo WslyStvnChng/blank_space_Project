07822559513dc5a6e6d538277e3e44c870fd1b16
Your Name <MarySimmons@Marys-MacBook-Pro.local>
2018-03-24 15:32:31 -0600

    added handlebars hashtags and improved handlebars docs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,37 +4,37 @@
 
 // Dependencies
 // =============================================================
-var path = require("path");
+var express = require("express");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+  // each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route
   app.get("/", function(req, res) {
-    res.render("index", {title: Express});
+    res.render("index", {title: express});
   });
 
   // contact route
   app.get("/contact", function(req, res) {
-    res.render("index", {title: Express});
+    res.render("index", {title: express});
   });
 
   // about route
   app.get("/about", function(req, res) {
-    res.render("about", {title: Express});
+    res.render("about", {title: express});
   });
 
   // categories route
   app.get("/categories", function(req, res) {
-    res.render("categories", {title: Express});
+    res.render("categories", {title: express});
   });
 
   // contact route
   app.get("/contact", function(req, res) {
-    res.render("index", {title: Express});
+    res.render("index", {title: express});
   });
 
 };
